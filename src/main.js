4a61456fc491f5c7e0b8e92c90cfb2765856bbd4
Thomas Cober <tcober5@gmail.com>
2019-10-06 20:40:36 -0500

    async loading fonts for performance

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
   head.link.push({
     rel: "stylesheet",
-    href: "https://use.typekit.net/blv1xtl.css"
+    href: "https://use.typekit.net/blv1xtl.css",
+    async: true
   });
 }
